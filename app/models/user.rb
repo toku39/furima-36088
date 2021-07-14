327da66490e1292c0a981099d57d9352d0575cb7
toku39 <86103018+toku39@users.noreply.github.com>
2021-07-14 18:29:38 +0900

    ユーザー管理機能　修正#2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validate :email
-
-    # NAME_REGIX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-    # NAMEKANA_REZIX = /\A[ァ-ヶー－]+\z/
-    # PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   with_options presence: true do
     validates :nickname
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '漢字・平仮名・カタカナ以外では登録できない' }
@@ -16,9 +11,10 @@ class User < ApplicationRecord
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ以外では登録できない' }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ以外では登録できない' }
     validates :birthday
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: '半角英数字でなければ登録できない' }
+    
   end
 
   # has_many :items
   # has_many :purchases
 end
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: '半角英数字でなければ登録できない' }
